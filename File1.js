6650d84917b212da83f3e97108eb2006bc7378fd
Pooja-manral <manralpooja2002@gmail.com>
2025-02-05 10:00:34 +0530

    bb

--- File1.js
+++ File1.js
@@ -1,3 +1,11 @@
 const fs= require('fs');
-const data = fs.readFileSync("./data.txt");
- console.log(data.toString());
+const data = fs.readFileSync("./data.txt","utf-8");
+//  console.log(data.toString());
+//  fs.writeFileSync("./data.txt","abes promise");
+//  fs.unlinkSync("./data.txt");
+ if(data.match("H")){
+     console.log("H is present")
+     const changedata =  data.replace("H","ABES");
+     fs.writeFileSync("./data.txt",changedata)
+ }
+ 
